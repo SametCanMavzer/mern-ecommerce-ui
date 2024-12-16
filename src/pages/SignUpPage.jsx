6ebbae4da625f2f8e8bd212bbd698792667f3f75
Samet Can Mavzer <sametmavzer6@gmail.com>
2024-12-16 16:19:41 +0300

    Add user store and API configurations

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import useUserStore from "../stores/useUserStore";
 
 
 const SignUpPage = () => {
@@ -12,9 +13,7 @@ const SignUpPage = () => {
 		confirmPassword: "",
 	});
 
-  const signup = null ;
-  const loading = false ;
-
+	const { signup, loading } = useUserStore();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+		// Proxy configuration to handle CORS issues by redirecting /api requests to backend server
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
