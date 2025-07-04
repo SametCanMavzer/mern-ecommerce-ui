f8b5d9a93882f3372c0059cd860b771a3e7eff2f
Samet Can Mavzer <sametmavzer6@gmail.com>
2025-07-04 14:35:59 +0300

    feat: add purchase cancel page with error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 
+//*pages
 import HomePage from "./pages/HomePage"
 import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
@@ -7,6 +8,7 @@ import AdminPage from "./pages/AdminPage"
 import CategoryPage from "./pages/CategoryPage"
 import CartPage from "./pages/CartPage"
 import PurchaseSuccessPage from "./pages/PurchaseSuccessPage";
+import PurchaseCancelPage from "./pages/PurchaseCancelPage";
 
 import Navbar from "./components/Navbar"
 import { Toaster } from "react-hot-toast"
@@ -51,8 +53,8 @@ function App() {
 			<Route path='/secret-dashboard'element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}/>
 			<Route path='/category/:category' element={<CategoryPage />} />
 			<Route path='/cart' element={user ? <CartPage /> : <Navigate to='/login' />} />
-			<Route path='/purchase-success'element={user ? <PurchaseSuccessPage /> : <Navigate to='/login' />}
-					/>
+			<Route path='/purchase-success'element={user ? <PurchaseSuccessPage /> : <Navigate to='/login' />} />
+			<Route path='/purchase-cancel' element={user ? <PurchaseCancelPage /> : <Navigate to='/login' />} />
 		</Routes>
 
 		</div>	
